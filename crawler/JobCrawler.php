e7124571c105f26720caab137e82a811bcbac7c1
anhdtuet1995 <anhdtuet1995@gmail.com>
2018-04-20 04:43:45 +0700

    add crawl post

--- crawler/JobCrawler.php
+++ crawler/JobCrawler.php
@@ -8,12 +8,37 @@
 
 namespace crawler;
 
+define('VIEC_LAM_24H', 'vieclam24h.vn');
+
 use DOMDocument;
 use DOMXPath;
 
 class JobCrawler {
 
     public static $instance;
+
+    //link to list type of jobs
+    public $seedUrl;
+    //type of job html tag
+    public $typeJobTag;
+
+    public $mContext;
+
+    //item tag
+    public $itemTag;
+    //detail of job link html tag
+    public $linkTag;
+    //title of job html tag
+    public $titleTag;
+    //company of job html tag
+    public $companyTag;
+    //location of job html tag
+    public $locationTag;
+    //salary of job html tag
+    public $salaryTag;
+
+    //list type of jobs
+    public $typeJobLinks;
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
@@ -22,23 +47,9 @@ class JobCrawler {
         return self::$instance;
     }
 
-    public function process($seedUrl, $typeJobTag) {
-        //get all type job links from $seedUrl
-        $typeJobLinks = $this->getAllTypeJobLinks($seedUrl, $typeJobTag);
-        print_r($typeJobLinks);
-        //with each type job url -> get all jobs from that type
-        foreach ($typeJobLinks as $link) {
-            $this->getJobsFromTypeJobLink($link);
-        }
-    }
+    public function __construct() {
 
-    public function getJobsFromTypeJobLink($typeJobLink) {
-
-    }
-
-    public function getAllTypeJobLinks($seedUrl, $typeJobTag) {
-        $links = array();
-        $ctx = stream_context_create(
+        $this->mContext = stream_context_create(
             array(
                 "ssl"=>array(
                     "verify_peer"=>false,
@@ -50,20 +61,97 @@ class JobCrawler {
             )
         );
 
-        $HTML = @file_get_contents($seedUrl,0, $ctx);
+    }
+
+    public function setDataForParseTypeOfJobs($url, $tag) {
+        $this->seedUrl = $url;
+        $this->typeJobTag = $this->handleTag($tag);
+    }
+
+    public function setDataForParseJobItem($itemTag, $linkTag, $titleTag, $companyTag, $locationTag, $salaryTag) {
+        $this->itemTag = $this->handleTag($itemTag);
+        $this->linkTag = $this->handleTag($linkTag);
+        $this->titleTag = $this->handleTag($titleTag);
+        $this->companyTag = $this->handleTag($companyTag);
+        $this->locationTag = $this->handleTag($locationTag);
+        $this->salaryTag = $this->handleTag($salaryTag);
+    }
+
+    public function process() {
+        //get all type job links from $seedUrl
+        $this->typeJobLinks = $this->getAllTypeJobLinks();
+
+        if (strpos($this->seedUrl, VIEC_LAM_24H) !== false) {
+            //link is vieclam24h.com
+            echo "a";
+            $this->getJobsViecLam24h();
+        }
+    }
+
+    public function getJobsViecLam24h() {
+        //with each type job url -> get all jobs from that type
+        //foreach ($this->typeJobLinks as $link) {
+        $link = $this->typeJobLinks[0];
+
+
+        $HTML = @file_get_contents($link,0, $this->mContext);
         $doc = new DOMDocument();
         @$doc->loadHTML($HTML);
         $selector = new DOMXPath($doc);
 
-        //handle type job tab for query
-        $query = $this->handleTypeJobTag($typeJobTag);
+        $result = $selector->query('//ul[contains(@class, "pagination")]/li/a');
+        foreach ($result as $res) {
+            if ($res->hasAttribute('onclick')) {
+                $temp = $res->getAttribute('onclick');
+                //get url from string
+                preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $temp, $match);
+                $url = $match[0][0];
+                //delete param lay_tin_mien_phi and page in html and set limit large
+                parse_str($url, $params);
+                $params['limit'] = 10000000;
+                unset($params['lay_tin_mien_phi']);
+                unset($params['page']);
+                print_r($url);
+                //getListJob
+                $jobHtml = @file_get_contents($url,0);
+                print_r($jobHtml);
+                @$doc->loadHTML($jobHtml);
+                $selector = new DOMXPath($doc);
+                $linkItems = $selector->query($this->linkTag);
+                $titleItems = $selector->query($this->titleTag);
+                $companyItems = $selector->query($this->companyTag);
+                $locationItems = $selector->query($this->locationTag);
+                $salaryItems = $selector->query($this->salaryTag);
 
-        $result = $selector->query($query);
-        print_r($query);
+                for ($i = 0; $i <= count($linkItems); $i++) {
+                    $linkItem = 'https://vieclam24h.vn/' . $linkItems[$i]->getAttribute('href');
+                    $titleItem = utf8_encode($titleItems[$i]->getAttribute('title'));
+                    $companyItem = utf8_encode($companyItems[$i]->nodeValue);
+                    $locationItem = utf8_encode($locationItems[$i]->nodeValue);
+                    $salaryItem = utf8_encode($salaryItems[$i]->nodeValue);
+
+                    print_r('('. $linkItem . ', ' . $titleItem . ', ' . $companyItem . ', '. $locationItem . ', ' . $salaryItem . ')' );
+                }
+                break;
+            }
+        }
+        //}
+    }
+
+
+
+    public function getAllTypeJobLinks() {
+        $links = array();
+        $HTML = @file_get_contents($this->seedUrl,0, $this->mContext);
+        $doc = new DOMDocument();
+        @$doc->loadHTML($HTML);
+        $selector = new DOMXPath($doc);
+
+        $result = $selector->query($this->typeJobTag);
         foreach ($result as $node) {
             $href = $node->getAttribute('href');
             if (!filter_var($href, FILTER_VALIDATE_URL)) {
-                $splitSeedUrl = explode('/', $seedUrl);
+                $splitSeedUrl = explode('/', $this->seedUrl);
                 $href = $splitSeedUrl[0] . '//' . $splitSeedUrl[2] . '/' . $href;
             }
             if (!in_array($href, $links)) {
@@ -74,7 +162,7 @@ class JobCrawler {
         return $links;
     }
 
-    private function handleTypeJobTag($typeJobTag) {
+    private function handleTag($typeJobTag) {
         $listTag = explode(" ", $typeJobTag);
         $strRes = "/";
         foreach ($listTag as $tag) {
--- index.php
+++ index.php
@@ -1,27 +1,16 @@
+
 <?php
 
 include "helpers/DBHelper.php";
 include "core/RequestRegular.php";
 include "core/Crawler.php";
-
+header("content-type: text/html; charset=UTF-8");
 set_time_limit(2147483647);
-
-//Line Break
-if (PHP_SAPI === 'cli'){
-    //if run from command line
-    $br = PHP_EOL;
-}
-else{
-    //if run from browser
-    $br = "<br/>";
-}
-
-$crawler = \core\Crawler::getInstance();
-$crawler->beginCrawl("https://vieclam24h.vn/");
-$aplopio = $crawler->getWebPagesJobType();
-foreach ($aplopio as $url => $properties) {
-    echo $url . $br;
-}
+$crawler = new \crawler\JobCrawler();
+//$crawler->getAllTypeJobLinks("https://www.careerlink.vn/", '//div[@id="search-by-category"]/ul/li/a');
+$crawler->setDataForParseTypeOfJobs("https://vieclam24h.vn/viec-lam-quan-ly", 'div.news-title  a');
+$crawler->setDataForParseJobItem('div.list-items', 'span.title-blockjob-main a', 'span.title-blockjob-main a', 'span.title-blockjob-sub a', 'span.onecol_province', 'div.note_mucluong');
+$crawler->process();
 
 //autoload of classes
 function __autoload($className) {
